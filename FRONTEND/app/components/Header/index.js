6d47ceddc146e944b7045deaddaabd32a9518f63
Gabriela <balicas.gb@gmail.com>
2019-06-03 12:19:30 -0700

    small changes before cleaning up the components in app

--- FRONTEND/app/components/Header/index.js
+++ FRONTEND/app/components/Header/index.js
@@ -18,7 +18,7 @@ function Header() {
         <HeaderLink to="/">
           <FormattedMessage {...messages.home} />
         </HeaderLink>
-        <HeaderLink to="/features">
+        <HeaderLink to="/currentMessages">
           <FormattedMessage {...messages.features} />
         </HeaderLink>
       </NavBar>
